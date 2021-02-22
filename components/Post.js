b3ee0a2c3be759e78cf0ac0aaba59430052a2ae8
ken505 <68226398+ken505@users.noreply.github.com>
2021-02-22 10:58:50 +0900

    change to function:

--- components/Post.js
+++ components/Post.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 // post で受け取った、 id と title を出力。
 // id と title は : で連結。
-const Post = ({ post }) => {
+export default function Post({ post }) {
   return (
     <div>
       <span>{post.id}</span>
@@ -11,12 +11,10 @@ const Post = ({ post }) => {
       {/* Blog 一覧から個別ページへ飛ぶための Link コンポーネント */}
       {/* 遷移先は動的に変わる id を指定。 */}
       <Link href={`/posts/${post.id}`}>
-      <span className="cursor-pointer text-blue-500 border-b border-blue-500 hover:bg-gray-200">
-        {post.title}
-      </span>
+        <span className="cursor-pointer text-blue-500 border-b border-blue-500 hover:bg-gray-200">
+          {post.title}
+        </span>
       </Link>
     </div>
   );
-};
-
-export default Post;
+}
